91d038663fb533ead5ecf9324e66ffc31d963363
Martin <Chatutie.Nkamanyimartin@tuni.fi>
2023-01-31 12:50:27 +0200

    Add code to temperature template

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a temperature: ";
+    int temperature = 0;
+    cin >> temperature;
+    cout << temperature <<" degrees Celsius is " << (temperature*1.8)+32 <<" degrees Fahrenheit" << endl;
+    cout << temperature <<" degrees Fahrenheit is " << (temperature-32)/1.8 <<" degrees Celsius" << endl;
+    // Write your code here
+
+    return 0;
+}
