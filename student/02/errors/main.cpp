bed40979127c1cb7f42fa1ad09051e90b1089c48
Martin <Chatutie.Nkamanyimartin@tuni.fi>
2023-01-31 14:18:32 +0200

    Add errors adjustment

--- student/02/errors/main.cpp
+++ student/02/errors/main.cpp
@@ -4,70 +4,120 @@
 using namespace std;
 
 
-int main()
-{
-    // get encryption key
+const int MAX_GUESSES = 10;
 
-    string key = "";
-    string message = "";
+// Prints empty lines at the beginning of execution
+void clean_screen();
 
-    cout << "Enter the encryption key: ";
+// Checks if all the letters include in the list of guessed letters
+bool is_word_already_guessed(string secret, string guessed);
 
-    getline(cin,key);
+// Prints string using '_' in the places of letters not guessed
+void print_game_status(string secret, string guessed);
 
-    // check length
-    if(key.length() < 26)
-    {
-        cout << "Error! The encryption key must contain 26 characters." << endl;
-        return EXIT_FAILURE;
+
+int main() {
+    string secret_word = "";
+    cout << "Give a word to be guessed: ";
+    getline(cin, secret_word);
+
+    clean_screen();
+
+    string guessed_letters = "";
+    int guesses_used = 0;
+
+    while ( not is_word_already_guessed(secret_word, guessed_letters)
+              and guesses_used < MAX_GUESSES ) {
+
+        cout << endl
+             << "Game status: ";
+        print_game_status(secret_word, guessed_letters);
+
+        if ( guessed_letters.size() > 0 ) {
+            cout << "You have used " << guesses_used
+                 << " guesses!"
+                 << endl
+                 << "You have guessed letters: " << guessed_letters
+                 << endl;
+        }
+
+        string guess = "";
+        cout << "Guess a letter: ";
+        getline(cin, guess);
+
+        if ( guess.size() != 1 ) {
+            cout << "You failed: give exactly one letter!"
+                 << endl;
+            continue;
+
+        } else if ( guessed_letters.find(guess) != string::npos ) {
+
+            cout << "You failed: you had already guessed the letter " << guess << "!"
+                 << endl;
+            continue;
+        }
+
+        guessed_letters += guess;
+
+        if ( secret_word.find(guess) == string::npos ) {
+            cout << "Guessed letter does not include the word!" << endl;
+        }
+
+        ++guesses_used;
     }
 
-    // check that all are lower case
-    for(unsigned int i = 0; i<key.length();i++)
-    {
-        if(!islower(key.at(i)))
-        {
-            cout << "Error! The text to be encrypted must contain only lower case characters." << endl;
-            return EXIT_FAILURE;
+    if ( not is_word_already_guessed(secret_word, guessed_letters) ) {
+        cout << endl
+             << "Guesses expired!"
+             << endl;
+
+    } else {
+
+        cout << endl
+             << "Right guess. You used " << guesses_used
+             << " guesses!"
+             << endl;
+    }
+}
+
+void clean_screen() {
+    // Cleaning screen by printing 100 empty lines.
+    for ( int i = 0; i < 100; ++i ) {
+        cout << endl;
+    }
+}
+
+
+bool is_word_already_guessed(string secret, string guessed) {
+    // Going through all the characters in secret string.
+    for ( string::size_type index = 0; index < secret.size(); ++index ) {
+        // If such a character in secret string is met that is not in
+        // guessed letters, it can be immediately concluded that
+        // the whole secret word has not yet guessed.
+        if ( guessed.find(secret.at(index)) == string::npos ) {
+            return false;
         }
     }
 
-    // get for only alphabelts
-    for (char c = 'a'; c < 'z'; ++c)
-    {
-        if (key.find(c) == string::npos)
-        {
-            cout << "Error! The encryption key must contain all alphabets a-z." << endl;
-            return EXIT_FAILURE;
+    // When all the secret string has been gone through, and each character
+    // was found from guessed string, it must hold that the whole
+    // secret word has been guessed.
+    return true;
+}
+
+
+void print_game_status(string secret, string guessed) {
+    // The characters of a string can be gone through also with such a for loop
+    // that looks like "for x in y" structure in Python.
+    // This is an alternative for the for loop with index variable
+    // used in previous function.
+    for ( char secret_char: secret ) {
+        if ( guessed.find(secret_char) == string::npos ) {
+            cout << "_";
+        } else {
+            cout << secret_char;
         }
     }
 
-    // encryption
-    cout << "Enter the text to be encrypted: ";
-    getline(cin, message);
-
-    // check that all are lower case
-    for(unsigned int i = 0; i<message.length();i++)
-    {
-        if(!islower(message.at(i)))
-        {
-            cout << "Error! The text to be encrypted must contain only lower case characters." << endl;
-            return EXIT_FAILURE;
-        }
-    }
-
-    string encrypted_message = "";
-    for(unsigned int i = 0; i < message.length(); ++i)
-    {
-        char character = message.at(i);
-        unsigned int ascii_value = character;
-        unsigned int position = ascii_value - static_cast<unsigned int>('a');
-        char encrypted_character = key.at(position);
-        encrypted_message += encrypted_character;
-    }
-
-
-    cout << "Encrypted text: " << encrypted_message << endl;
-
-    return EXIT_SUCCESS;
+    cout << endl;
 }
